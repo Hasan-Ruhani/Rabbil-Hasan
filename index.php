e47f48d0cad79701467a0c10e41755d3afc7c72a
H A S A N <hasan25042019@gmail.com>
2023-05-01 22:13:55 +0600

    Update index.php
    yyuyu

--- index.php
+++ index.php
@@ -1,24 +1,32 @@
 <html>
   <head>
-
+    <title>Calculator</title>
   </head>
 
   <body>
     
      <script>
-      let num = 10;
-
-for (let i = 0; i < num; i++) {
-
-console.log(i);
-
-if (i == 5) {
-
-break;
-
-}
-
-}
+        function gradeCalculator(grade) {
+          if (isNaN(grade) || grade < 0 || grade > 100) {
+            return "Error:  Input must be a number between 0  - 100";
+          } 
+          else if (grade >= 90) {
+            return "A";
+          } 
+          else if (grade >= 80) {
+            return "B";
+          } 
+          else if (grade >= 70) {
+            return "C";
+          } 
+          else if (grade >= 60) {
+            return "D";
+          } 
+          else {
+            return "F";
+          }
+        }
+        document.write(gradeCalculator(75));
      </script>
   </body>
 </html>
