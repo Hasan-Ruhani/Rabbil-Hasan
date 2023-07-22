d404c9c80ff1dcf71aea936b404cc0638a323028
H A S A N <hasan25042019@gmail.com>
2023-07-22 22:04:02 +0600

    Update JWTToken.php
    cvc

--- Module-20/inventory/app/Helper/JWTToken.php
+++ Module-20/inventory/app/Helper/JWTToken.php
@@ -1,26 +1,27 @@
 <?php 
 namespace App\Helper;
 
-use Exception;
 use Firebase\JWT\key;
 use Firebase\JWT\JWT;
-use FFI\Exception;
+use Exception;
+
 class JWTToken{
-    public static function CreateToken($userID):string{
+    public static function CreateToken($userEmail):string{
         $key = env('JWT_KEY');                // JWT_KEY inside the .env file
         $payload = [
             'iss' => "Laravel-jwt",
             'ist' => time(),
             'exp' => time() + 60*60,
-            'user' => $userID
+            'userEmail' => $userEmail
         ];
         return JWT::encode($payload, $key, 'HS256');
     }
 
     public static function DecodeToken($token){
         try{
+            $key = env('JWT_KEY');                // JWT_KEY inside the .env file
             $decoded = JWT::decode($token, new key($key, 'HS256'));
-            return $decoded->userID;
+            return $decoded->userEmail;
         }
         catch(Exception $e){
             return 'unauthorize';
